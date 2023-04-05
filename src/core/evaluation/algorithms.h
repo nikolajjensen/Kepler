b2a9daf8ab36eae4ae77dd6662bae1a1e741a023
Nikolaj B. Jensen <nikko4913@icloud.com>
2023-04-05 04:04:25 +0100

    More comments

--- src/core/evaluation/algorithms.h
+++ src/core/evaluation/algorithms.h
@@ -22,14 +22,93 @@
 #include "core/array.h"
 
 namespace kepler {
+
+    /**
+     * Returns the floor of the given number.
+     *
+     * Note that this function is defined for complex numbers.
+     * The definition follows the one published as part of J's implementation.
+     *
+     * Source: https://www.jsoftware.com/papers/eem/complexfloor1.htm#0
+     *
+     * @param number The number to take the floor of.
+     * @return The result of taking the floor of the given number.
+     */
     Number floor(const Number& number);
+
+    /**
+     * Returns the binomial of two numbers.
+     *
+     * This function is defined in terms of the gamma function,
+     * in a similar way to the factorial function.
+     *
+     * Source: https://help.dyalog.com/latest/Content/Language/Primitive%20Functions/Binomial.htm
+     *
+     * @param alpha The left argument.
+     * @param omega The right argument.
+     * @return The binomial of the two numbers.
+     */
     Number binomial(const Number& alpha, const Number& omega);
 
+    /**
+     * Encloses elements in the subject array according to the partitioning.
+     *
+     * The behaviour is described in detail on this page:
+     * https://aplwiki.com/wiki/Partitioned_Enclose
+     *
+     * @param partitioning The partitioning to use.
+     * @param subject The array to enclose.
+     * @return The result of the partitioned enclose operation.
+     */
     Array partitioned_enclose(const Array& partitioning, const Array& subject);
+
+    /**
+    * Encloses elements in the subject string according to the partitioning.
+    *
+    * The behaviour is described in detail on this page:
+    * https://aplwiki.com/wiki/Partitioned_Enclose
+    *
+    * @param partitioning The partitioning to use.
+    * @param subject The string to enclose.
+    * @return The result of the partitioned enclose operation.
+    */
     Array partitioned_enclose(const Array& partitioning, const String& subject);
 
+    /**
+     * Returns an Array which consists of all elements in alpha, except those in omega.
+     *
+     * The behaviour is described in detail on this page:
+     * https://aplwiki.com/wiki/Without
+     *
+     * @param alpha The array to remove elements from.
+     * @param omega The array of elements to remove.
+     * @return The result of the without operation.
+     */
     Array without(const Array& alpha, const Array& omega);
+
+    /**
+     * Returns a String which consists of all symbols in alpha, except those in omega.
+     *
+     * The behaviour is described in detail on this page:
+     * https://aplwiki.com/wiki/Without
+     *
+     * @param alpha The String to remove symbols from.
+     * @param omega The String of symbols to remove.
+     * @return The result of the without operation.
+     */
     Array without(const String& alpha, const String& omega);
 
+    /**
+     * Performs a reshape operation. A new array is created which has the shape list
+     * of alpha, and the elements of omega. If omega does not have enough elements,
+     * elements are reused from the beginning.
+     *
+     * The behaviour is described in detail on this page:
+     * https://aplwiki.com/wiki/Reshape
+     *
+     * @param alpha The Array containing the shape list.
+     * @param omega The Array containing the elements to use as fillers.
+     * @return The resulting array.
+     */
     Array rho(const Array& alpha, const Array& omega);
 };
--- src/core/evaluation/operations/pervade.h
+++ src/core/evaluation/operations/pervade.h
@@ -23,19 +23,71 @@
 
 namespace kepler {
 
+    /**
+     * Mixin class for operations that pervade over an array.
+     *
+     * This means that if an operation inherits from PervadeMixin, and
+     * does not implement any of the operator()(Array) methods, it will
+     * be applied between each element of the array, in the traditional
+     * APL way.
+     */
     template <typename BASE>
     struct PervadeMixin : BASE {
         using BASE::BASE;
         using BASE::operator();
 
+        /**
+         * Pervade over the Array.
+         *
+         * This applies itself (this) to each element of the Array.
+         *
+         * @param omega The Array to apply the operation to.
+         * @return A new Array with the result.
+         */
         virtual Array operator()(const Array& omega);
+
+        /**
+         * Pervade over two Arrays.
+         *
+         * This applies itself (this) between each pair of elements of alpha and omega.
+         *
+         * If the arrays are equally sized, the operation is applied to each pair of elements.
+         * If alpha is scalar, the operation is applied between alpha and every element of omega.
+         * If omega is scalar, the operation is applied between every element of alpha and omega.
+         *
+         * @param alpha The left Array.
+         * @param omega The right Array.
+         * @return A new Array with the result.
+         */
         virtual Array operator()(const Array& alpha, const Array& omega);
+
+        /**
+         * Pervade over two Strings.
+         *
+         * This applies itself (this) between each pair of Chars of alpha and omega.
+         *
+         * If the Strings are equally sized, the operation is applied to each pair of Chars.
+         * If alpha is scalar, the operation is applied between alpha and every Char of omega.
+         * If omega is scalar, the operation is applied between every Char of alpha and omega.
+         *
+         * @param alpha The left String.
+         * @param omega The right String.
+         * @return A new Array with the result.
+         */
         virtual Array operator()(const String& alpha, const String& omega);
 
     private:
+        /**
+         * Apply the operation to the single element in the Array.
+         */
         Array apply(const Array::element_type& omega);
+
+        /**
+         * Apply the operation to the single element in two Arrays.
+         */
         Array apply(const Array::element_type& alpha, const Array::element_type& omega);
     };
 };
 
+// Include of .tpp file goes at the bottom.
 #include "pervade.tpp"
